46536e58eb5ab26f287ffada700e126a4df52a5d
Sun Davey <sundavey@hotmail.com>
2018-06-29 17:05:37 -0700

    reversing each words on a string

--- c#/Program.cs
+++ c#/Program.cs
@@ -16,17 +16,20 @@ namespace c_
             // string [] input = {"kumar", "Mishra" , "Hello" , "Hi", "good", "day", "Bye"};
             // printOutput(input);
             // System.Console.WriteLine(ReverseString("hello"));
-            System.Console.WriteLine(consecutiveStr("aaaaaabbcbbbbbcbbbb"));
+            // System.Console.WriteLine(consecutiveStr("aaaaaabbcbbbbbcbbbb"));
 
 
 
-            int[] nuts = new int[] { 1, 5, 4, 2, 7, 35 };
-            int[] bolts = new int[] { 35, 2, 4, 5, 1, 7 };
-            Sort(nuts, bolts);
-            System.Console.WriteLine(OneAway("pale", "ple"));
-            System.Console.WriteLine(OneAway("pales", "pale"));
-            System.Console.WriteLine(OneAway("pale", "bale"));
-            System.Console.WriteLine(OneAway("pale", "bake"));
+            // int[] nuts = new int[] { 1, 5, 4, 2, 7, 35 };
+            // int[] bolts = new int[] { 35, 2, 4, 5, 1, 7 };
+            // Sort(nuts, bolts);
+            // System.Console.WriteLine(OneAway("pale", "ple"));
+            // System.Console.WriteLine(OneAway("pales", "pale"));
+            // System.Console.WriteLine(OneAway("pale", "bale"));
+            // System.Console.WriteLine(OneAway("pale", "bake"));
+
+            System.Console.WriteLine(StrCompression("aabcccccaaa"));
+            System.Console.WriteLine(reverseWord("Life is Good"));
         }
 
         static void countAlpabet(String input)
@@ -131,13 +134,11 @@ namespace c_
                             count = 1;
                             continue;
                         }
-
                     }
                     else
                     {
                         dic.Add(str[i], count);
                         count = 1;
-
                     }
                 }
             }
@@ -176,54 +177,93 @@ namespace c_
         {
             int len1 = str1.Length;
             int len2 = str2.Length;
-            if(Math.Abs(len1 - len2) > 1) return false;
-            
-                string shorter = str1.Length > str2.Length ? str2 : str1;
-                string longer = str1.Length > str2.Length ? str1 : str2;
-                int idx1 = 0;
-                int idx2 = 0;
-                bool foundDiff = false;
-                while (idx1 < str1.Length && idx2 < str2.Length)
+            if (Math.Abs(len1 - len2) > 1) return false;
+
+            string shorter = str1.Length > str2.Length ? str2 : str1;
+            string longer = str1.Length > str2.Length ? str1 : str2;
+            int idx1 = 0;
+            int idx2 = 0;
+            bool foundDiff = false;
+            while (idx1 < str1.Length && idx2 < str2.Length)
+            {
+                if (shorter[idx1] != longer[idx2])
                 {
-                    // if(shorter.Length + 1 != longer.Length) return false;
+                    System.Console.WriteLine(str1[idx1] + " " + str2[idx2]);
                     if (shorter[idx1] != longer[idx2])
                     {
-                        System.Console.WriteLine(str1[idx1] + " " +str2[idx2]);
-                        if(shorter[idx1] != longer[idx2]){
-                            if (foundDiff) return false;
-                            foundDiff = true;
-                            if(shorter.Length == longer.Length){
-                                idx1++;
-                            }
+                        if (foundDiff) return false;
+                        foundDiff = true;
+                        if (shorter.Length == longer.Length)
+                        {
+                            idx1++;
                         }
-                        // System.Console.WriteLine("changed to true");
                     }
-                    else
-                    {
-                        idx1++;
-                    }
-                    idx2++;
                 }
-            // }
-            // else
-            // {
-            //     int count = 0;
-            //     for (int i = 0; i < len1; i++)
-            //     {
-            //         if (str1[i] != str1[i])
-            //         {
-            //             count++;
-            //         }
-            //         if (count > 1)
-            //         {
-            //             System.Console.WriteLine("more than one diff");
-            //             return false;
-                        
-            //         }
-            //     }
-            // }
-            // System.Console.WriteLine("this should be true");
+                else
+                {
+                    idx1++;
+                }
+                idx2++;
+            }
             return true;
+        }
+
+        static string StrCompression(string str)
+        {
+            int count = 1;
+            string result = "";
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i - 1] != str[i])
+                {
+                    result += (str[i - 1] + count.ToString());
+                    count = 1;
+                }
+                else if (i == str.Length - 1)
+                {
+                    count++;
+                    result += (str[i - 1] + count.ToString());
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return result.Length > str.Length ? str : result;
+        }
+
+
+        static string reverseWord(string str)
+        {
+            int start = 0;
+            int end = 0;
+            int space = 0;
+            string newStr = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    space = i;
+                    end = i - 1;
+                }
+                if (i == str.Length - 1)
+                {
+                    end = str.Length - 1;
+                }
+                if(space != 0){
+                    for (int j = end; j >= start; j--)
+                    {
+                        newStr += str[j];
+                        if (j == start)
+                        {
+                            newStr += " ";
+                        }
+                    }
+                    start = space + 1;
+
+                }
+            }
+            return newStr;
         }
     }
 }
