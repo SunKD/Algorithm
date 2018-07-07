0aae7a1460db3e8664f999f470f9033ce13e93be
Sun Davey <sundavey@hotmail.com>
2018-07-07 15:41:19 -0700

    comments added

--- isAnagram.js
+++ isAnagram.js
@@ -1,7 +1,9 @@
 function isAnagram(str1, str2){
+    //breaking the string by char and putting them into an array
     var char1 = [...str1];
     var char2 = [...str2];
 
+    //.sort() will sort the characters alphabetical order
     char1.sort();
     char2.sort();
     console.log(char1)
